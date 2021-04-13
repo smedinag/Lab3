f943b4f283d7c8580a8241369ecb2d4cfc0ead86
Susana Medina <smedinag@usbcali.edu.co>
2021-04-13 13:12:49 -0500

    BD en postgres creada

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'webapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'webappdb', ## nombre de la base de datos
+        'USER': 'postgres',
+        'PASSWORD': 'blaumond',
+        'HOST': '127.0.0.1', ## localhost
+        'PORT': '5432',
     }
 }
 
